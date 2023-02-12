ea439364072a85c994ee5bb087512a23674503a4
Sachin Prasad <sachin.prasad@xlscout.com>
2023-02-12 19:53:14 +0530

    complete get subtitle api

---
+++ app.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, request
+from youtube_transcript_api import YouTubeTranscriptApi
+app = Flask(__name__)
+
+
+@app.route('/getcaption', methods=['POST'])
+def post_data():
+    try:
+        data = request.get_json()
+        srt = YouTubeTranscriptApi.get_transcript(data["id"])
+        print(data["id"])
+        # Do something with the received data
+        return srt
+    except Exception as e:
+        return 'Error: {}'.format(str(e)), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
